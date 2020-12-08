cdaa7caa93051ba4a2290d2ec266349ede53db68
unknown <mario.akhras@gmail.com>
2020-12-08 11:38:03 -0500

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Agua Viva
+        </q-toolbar-title>
+
+        <!-- <q-btn round dense flat icon="menu" /> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+
+    <q-drawer
+      v-model="rightDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Countries',
+    caption: 'All countries',
+    link: 'countries',
+    icon: 'earth',
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      rightDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Countries.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page>
+  <q-header>
+    <q-toolbar>
+      <q-btn rounded flat dense icon="mdi-arrow-left" label="return" @click="$router.push({name: 'home'})" />
+      <q-space />
+    </q-toolbar>
+  </q-header>
+    <div>Here be all countries...</div>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
